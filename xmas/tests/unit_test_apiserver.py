8f967c80672a69320dd4261bae3a8fc8c9ee1b93
pikesley <sam.pikesley@gmail.com>
2019-12-14 17:41:12 +0000

    clean up the API

--- xmas/tests/unit_test_apiserver.py
+++ xmas/tests/unit_test_apiserver.py
@@ -1,8 +1,10 @@
 import json
+from unittest.mock import MagicMock
 
 from starlette.testclient import TestClient
 
-from apiserver import app
+from apiserver import APP
+from workers.light_worker import light_one, light_all
 
 
 class TestApiServer:
@@ -10,7 +12,9 @@ class TestApiServer:
 
     def setup_method(self):
         """Do some initialisation."""
-        self.client = TestClient(app)  # pylint: disable=W0201
+        self.client = TestClient(APP)  # pylint: disable=W0201
+        light_one.delay = MagicMock()
+        light_all.delay = MagicMock()
 
     def test_all(self):
         """Test `/lights/all`."""
@@ -24,34 +28,40 @@ class TestApiServer:
         """Test `/lights/all`."""
         payload = json.dumps({'colour': 'mauve'})
         response = self.client.post('/lights/all', data=payload)
+
         assert response.status_code == 422
         assert response.json() == {'detail': "Unknown colour 'mauve'"}
 
     def test_one(self):
-        """Test `/lights/single`."""
-        payload = json.dumps({'colour': 'green', 'index': 3})
-        response = self.client.post('/lights/single', data=payload)
+        """Test `/lights/single/{index}`."""
+        payload = json.dumps({'colour': 'green'})
+        response = self.client.post('/lights/single/3', data=payload)
+
         assert response.status_code == 200
         assert response.json() == {'colour': 'green', 'index': 3}
 
     def test_one_with_bad_colour(self):
-        """Test `/lights/single`."""
-        payload = json.dumps({'colour': 'violet', 'index': 5})
-        response = self.client.post('/lights/single', data=payload)
+        """Test `/lights/single/{index}`."""
+        payload = json.dumps({'colour': 'violet'})
+        response = self.client.post('/lights/single/5', data=payload)
+
         assert response.status_code == 422
         assert response.json() == {'detail': "Unknown colour 'violet'"}
 
     def test_one_with_bad_index(self):
-        """Test `/lights/single`."""
-        payload = json.dumps({'colour': 'yellow', 'index': 99})
-        response = self.client.post('/lights/single', data=payload)
+        """Test `/lights/single/{index}`."""
+        payload = json.dumps({'colour': 'yellow'})
+        response = self.client.post('/lights/single/99', data=payload)
+
         assert response.status_code == 422
         assert response.json() == {'detail': 'Invalid index 99'}
 
     def test_one_with_bad_index_and_bad_colour(self):
-        """Test `/lights/single`."""
-        payload = json.dumps({'colour': 'grey', 'index': 99})
-        response = self.client.post('/lights/single', data=payload)
+        """Test `/lights/single/{index}`."""
+        payload = json.dumps({'colour': 'scarlet'})
+        response = self.client.post('/lights/single/99', data=payload)
+
         assert response.status_code == 422
-        assert response.json() == {'detail': "Unknown colour 'grey'"}
-        # this should be richer
+        assert response.json() == {
+            'detail': "Invalid index 99, Unknown colour 'scarlet'"
+        }
